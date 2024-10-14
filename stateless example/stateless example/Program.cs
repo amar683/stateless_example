6d44c73d4a0f9e7c38adb2693248e21d8e87f87a
amar683 <69433283+amar683@users.noreply.github.com>
2024-10-15 04:56:45 +0530

    Add files via upload

---
+++ stateless example/stateless example/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stateless;
+
+namespace stateless_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new orderStates();
+
+            order.DisplayState();
+
+            order.fire(orderStates.Trigger.pay);
+            order.fire(orderStates.Trigger.ship);
+            order.fire(orderStates.Trigger.deliver);
+
+            var newOrder = new orderStates();
+            newOrder.fire(orderStates.Trigger.deliver);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ stateless example/stateless example/orderStates.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stateless;
+
+namespace stateless_example
+{
+    class orderStates
+    {
+        public enum orderState
+        {
+            created,
+            paid,
+            shipped,
+            delivered
+        }
+        public enum Trigger
+        {
+            pay,
+            ship,
+            deliver
+        }
+        private StateMachine<orderState, Trigger> _machine;
+        public orderStates()
+        {
+            _machine = new StateMachine<orderState, Trigger>(orderState.created);
+
+            _machine.Configure(orderState.created).Permit(Trigger.pay, orderState.paid);
+            _machine.Configure(orderState.paid).Permit(Trigger.ship, orderState.shipped);
+            _machine.Configure(orderState.created).Permit(Trigger.deliver, orderState.delivered);
+
+        }
+        public orderState GetState() => _machine.State;
+        public void fire(Trigger trigger)
+        {
+            try
+            {
+                _machine.Fire(trigger);
+                Console.WriteLine($"Order state changed to: {_machine.State}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Invalid transition: {ex.Message}");
+            }
+        }
+        public void DisplayState()
+        {
+            Console.WriteLine($"Current order state: {_machine.State}");
+        }
+    }
+}
